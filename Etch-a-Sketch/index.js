b8c83c0d1863a2edbf951578bad627cccf2fd970
Pobitro Bhattacharya <bhattacharyapobitro@gmail.com>
2025-05-19 17:17:48 +0530

    New and updated

--- Etch-a-Sketch/index.js
+++ Etch-a-Sketch/index.js
@@ -1,5 +1,16 @@
-let body = document.querySelector("body")
-for (let index = 0; index < 256; index++) {
-    let row = document.createElement("div");
-    body.appendChild(row);    
-}
+let num = prompt("Enter the size of square: ")
+function gridmaker(num = 16){
+    let body = document.querySelector("body")
+    for (let index = 0; index < num**2; index++) {
+        let row = document.createElement("div");
+
+
+    row.addEventListener("click", () => {
+    row.style.backgroundColor = 
+        row.style.backgroundColor === "black" ? "white" : "black";
+    });
+        body.appendChild(row);
+        body.style.gridTemplateRows = `repeat(${num},22px)`;
+        body.style.gridTemplateColumns = `repeat(${num},22px)`;     
+    }}
+gridmaker(num);
